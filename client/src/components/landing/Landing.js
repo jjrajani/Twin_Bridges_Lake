9263614700d4637626a998e2941e16b036577d17
humdrum <jjrajani@gmail.com>
2017-08-23 17:07:27 -0400

    landing footer

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
 const Landing = () => {
-  return <div />;
+  return (
+    <div className="landing">
+      <div className="header center col">
+        <h1>Twin Bridges Lake</h1>
+        <h2>Family Fishing & Monster Catfish</h2>
+      </div>
+      <div className="center col">
+        <h3>Recently Caught</h3>
+        <h3>Location</h3>
+        <h3>Admissions</h3>
+      </div>
+      <div className="footer">
+        <h4>1119 Braselton Hwy, Lawrenceville Ga, 30043</h4>
+        <h4>(770) 963-6739</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
-
-// <div>
-//   <h1>Twin Bridges Lake</h1>
-//   <h2>Family Fishing & Monster Catfish</h2>
-// </div>
-// <div>
-//   <h3>Recently Caught</h3>
-//   <h3>Location</h3>
-//   <h3>Admissions</h3>
-// </div>
-// <div>
-//   <h4>1119 Braselton Hwy, Lawrenceville Ga, 30043</h4>
-//   <h4>(770) 963-6739</h4>
-// </div>
