ef4cca5fe60146aeb0da453f51588575eff12bb0
humdrum <jjrajani@gmail.com>
2017-08-28 16:16:06 -0400

    landing styles

--- client/src/components/landing/Title.js
+++ client/src/components/landing/Title.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title center col">
+      <div className="overlay" />
       <h1>Twin Bridges Lake</h1>
       <h2>Family Fishing & Monster Catfish</h2>
     </div>
