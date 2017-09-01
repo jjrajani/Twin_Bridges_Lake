5520289f4e3890a10a39fc49fd4ad1128cc9429f
humdrum <jjrajani@gmail.com>
2017-09-01 14:05:51 -0400

    fish stock refac

--- client/src/components/fish_stock/FishStock.js
+++ client/src/components/fish_stock/FishStock.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Stocked, Native } from './components';
 
 const FishStock = () => {
   return (
@@ -6,46 +7,8 @@ const FishStock = () => {
       <div className="content">
         <h3 className="sub_header">Fish in the Lake</h3>
         <ul className="list">
-          <ul className="sub_list centered">
-            <li className="list_header">
-              <h3>Stocked</h3>
-            </li>
-            <li className="list-item">
-              <p>
-                <span className="small">Bluehead Catfish</span>
-              </p>
-            </li>
-            <li className="list-item">
-              <p>
-                <span className="small">Channel Catfish</span>
-              </p>
-            </li>
-          </ul>
-          <ul className="sub_list centered">
-            <li className="list_header">
-              <h3>Native</h3>
-            </li>
-            <li className="list-item">
-              <p>
-                <span className="left">Large Mouth Bass</span>
-              </p>
-            </li>
-            <li className="list-item">
-              <p>
-                <span className="left">Bream</span>
-              </p>
-            </li>
-            <li className="list-item">
-              <p>
-                <span className="left">Crappie</span>
-              </p>
-            </li>
-            <li className="list-item">
-              <p>
-                <span className="left">Carp</span>
-              </p>
-            </li>
-          </ul>
+          <Stocked />
+          <Native />
         </ul>
       </div>
     </div>
---
+++ client/src/components/fish_stock/components/index.js
@@ -0,0 +1,4 @@
+import Stocked from './Stocked';
+import Native from './Native';
+
+export { Stocked, Native };
