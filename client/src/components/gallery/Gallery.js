82713d0f28e6b21420a1ee9142f416c0a3ea9395
humdrum <jjrajani@gmail.com>
2017-09-01 12:45:01 -0400

    gallery styles

--- client/src/components/gallery/Gallery.js
+++ client/src/components/gallery/Gallery.js
@@ -2,15 +2,8 @@ import React, { Component } from 'react';
 import * as actions from '../../actions';
 import { connect } from 'react-redux';
 import ZoomImage from './components/ZoomImage';
-import ScrollTo from './components/ScrollTo';
 
 class Gallery extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      scrollY: 0
-    };
-  }
   componentDidMount() {
     this.props.fetchPics();
   }
@@ -19,34 +12,34 @@ class Gallery extends Component {
   }
   selectImage(url) {
     this.props.selectImage(url);
-    this.setState({
-      scrollY: window.scrollY
-    });
     this.props.toggleModal();
   }
   render() {
     return (
-      <ScrollTo y={this.state.scrollY}>
-        <div className="gallery">
-          <ZoomImage
-            close={this.closeModal.bind(this)}
-            showClass={this.props.showGalleryModal === true ? '' : 'hidden'}
-            image={this.props.gallery.currentImage}
-          />
-          <ul className="image-list">
-            {this.props.pics.map(p => {
-              return (
-                <li
-                  key={p}
-                  className="image-item"
-                  style={{ backgroundImage: `url(${p})` }}
-                  onClick={this.selectImage.bind(this, p)}
-                />
-              );
-            })}
-          </ul>
+      <div className="main-content our_story">
+        <div className="content has_fixed_sub_header">
+          <h3 className="fixed sub_header">Gallery</h3>
+          <div className="gallery">
+            <ZoomImage
+              close={this.closeModal.bind(this)}
+              showClass={this.props.showGalleryModal === true ? '' : 'hidden'}
+              image={this.props.gallery.currentImage}
+            />
+            <ul className="image-list">
+              {this.props.pics.map(p => {
+                return (
+                  <li
+                    key={p}
+                    className="image-item"
+                    style={{ backgroundImage: `url(${p})` }}
+                    onClick={this.selectImage.bind(this, p)}
+                  />
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </ScrollTo>
+      </div>
     );
   }
 }
