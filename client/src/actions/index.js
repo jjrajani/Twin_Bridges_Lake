7ee8ab5350684aaaf201c2d7c6cabcb1a473ec45
humdrum <jjrajani@gmail.com>
2017-09-14 16:20:31 -0400

    remove revies actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,17 +1,11 @@
 import * as authActions from './auth/actions';
-import * as reviewActions from './review/actions';
-import * as reviewsActions from './reviews/actions';
 import * as galleryActions from './gallery/actions';
-import * as selectStarsActions from './selectStars/actions';
 import * as picsActions from './pics/actions';
 import * as locationActions from './location/actions';
 import * as rulesActions from './rules/actions';
 
 export {
   authActions,
-  reviewActions,
-  reviewsActions,
-  selectStarsActions,
   galleryActions,
   picsActions,
   locationActions,
