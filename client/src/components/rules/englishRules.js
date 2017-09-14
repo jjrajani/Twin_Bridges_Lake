b19e0fb2622a83591fb9e3acc368e97ff78d2720
humdrum <jjrajani@gmail.com>
2017-09-14 16:27:00 -0400

    rules typos

--- client/src/components/rules/englishRules.js
+++ client/src/components/rules/englishRules.js
@@ -6,9 +6,9 @@ const ENGLISH_RULES = [
     All vehicles/customers <span className="bold">
       check-in & check-out
     </span>{' '}
-    with management inside bait shop
+    with management inside bait shop.
   </p>,
-  <p>Three Rod limit per customer</p>,
+  <p>Three Rod limit per customer.</p>,
   <p>
     No throw lines, no net fishing, no snagging; Treble hooks allowed only on
     artificial lures.
@@ -20,7 +20,7 @@ const ENGLISH_RULES = [
   <p>
     Family atmosphere is maintained on lake at all times. Customers in violation
     of this rule by way of excessive alcohol use, profanity, loud music,
-    disturbances, or illegal activiy of any type will be required to leave.{' '}
+    disturbances, or illegal activiy of any type will be required to leave.
   </p>,
   <p>
     No cleaning of fish on premises. Please clean up your area during and after
@@ -29,11 +29,11 @@ const ENGLISH_RULES = [
   <p>No firearms</p>,
   <p>
     No open ground fires allowed. We provide burn barrels if you would like to
-    have a fire
+    have a fire.
   </p>,
-  <p>Restrooms provided must be used</p>,
+  <p>Restrooms provided must be used.</p>,
   <p>
-    Management reserves the right ot deny enterance or refuse service to anyone
+    Management reserves the right to deny entrance or refuse service to anyone
     for any reason.
   </p>,
   <p>
