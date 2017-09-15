526133d898a274ee0c57386d885e288525816554
humdrum <jjrajani@gmail.com>
2017-09-15 11:29:20 -0400

    changed the location icon.  added a footer;

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const Footer = () => {
-    return <div id="footer">Footer</div>;
+    return (
+        <div id="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-6">
+                        <p>&copy; Twin Bridges Lake</p>
+                    </div>
+                    <div className="col-xs-6 right_align">
+                        <p>
+                            Built by{' '}
+                            <a href="https://jjrajani.github.io/#/home">
+                                humdrum
+                            </a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Footer;
--- client/src/components/home/components/Contact.jsx
+++ client/src/components/home/components/Contact.jsx
@@ -6,43 +6,44 @@ const ScrollElement = Scroll.Element;
   see src/components/nav/components/HomeDropDown
 */
 const Contact = () => {
-  return (
-    <ScrollElement name="contact" id="contact">
-      <div className="row">
-        <hr className="hr-extra-bottom-padding" />
-        <div className="col-md-12">
-          <h1>
-            <i className="fa fa-mobile" />
-            Contact
-          </h1>
-          <p className="blurb_pad">
-            Please feel free to contact us if you have any questions, comments,
-            or concerns.
-          </p>
-          <div className="contact_info">
-            <div className="sub_info">
-              <i className="small_fa fa fa-phone" />
-              <p>(770) 963-6739</p>
+    return (
+        <ScrollElement name="contact" id="contact">
+            <div className="row">
+                <hr className="hr-extra-bottom-padding" />
+                <div className="col-md-12">
+                    <h1>
+                        <i className="fa fa-mobile" />
+                        Contact
+                    </h1>
+                    <p className="blurb_pad">
+                        Please feel free to contact us if you have any
+                        questions, comments, or concerns.
+                    </p>
+                    <div className="contact_info">
+                        <div className="sub_info">
+                            <i className="small_fa fa fa-phone" />
+                            <a href="tel:7709636739">(770) 963-6739</a>
+                        </div>
+                        <div className="sub_info">
+                            <i className="small_fa fa fa-map-marker" />
+                            <p>
+                                <span>
+                                    1119 Braselton Hwy <br /> Lawrenceville, Ga
+                                    30043
+                                </span>
+                            </p>
+                        </div>
+                        <div className="sub_info">
+                            <i className="small_fa fa fa-envelope" />
+                            <a href="mailto:twin.bridges.lake.fishing@gmail.com">
+                                twin.bridges.lake.fishing@gmail.com
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="sub_info">
-              <i className="small_fa fa fa-globe" />
-              <p>
-                <span>
-                  1119 Braselton Hwy <br /> Lawrenceville, Ga 30043
-                </span>
-              </p>
-            </div>
-            <div className="sub_info">
-              <i className="small_fa fa fa-envelope" />
-              <a href="mailto:twin.bridges.lake.fishing@gmail.com">
-                twin.bridges.lake.fishing@gmail.com
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </ScrollElement>
-  );
+        </ScrollElement>
+    );
 };
 
 export default Contact;
