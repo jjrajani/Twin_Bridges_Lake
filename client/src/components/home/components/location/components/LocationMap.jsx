5efd1ead3420f516e0aa4964fb43cd6bb70a6e80
humdrum <jjrajani@gmail.com>
2017-09-15 11:37:59 -0400

    location map zoom level depending on screen size;

--- client/src/components/home/components/location/components/LocationMap.jsx
+++ client/src/components/home/components/location/components/LocationMap.jsx
@@ -1,42 +1,44 @@
 import React from 'react';
 import { compose, withStateHandlers } from 'recompose';
 import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker,
-  InfoWindow
+    withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+    InfoWindow
 } from 'react-google-maps';
 import MapLabel from './MapLabel.js';
+const windowWidth = window.innerWidth;
+const zoomLevel = windowWidth < 576 ? 13 : 14;
 
 const LocationMap = compose(
-  withStateHandlers(
-    () => ({
-      isOpen: false
-    }),
-    {
-      toggleInfoWindow: ({ isOpen }) => () => ({
-        isOpen: !isOpen
-      })
-    }
-  ),
-  withScriptjs,
-  withGoogleMap
+    withStateHandlers(
+        () => ({
+            isOpen: false
+        }),
+        {
+            toggleInfoWindow: ({ isOpen }) => () => ({
+                isOpen: !isOpen
+            })
+        }
+    ),
+    withScriptjs,
+    withGoogleMap
 )(props =>
-  <GoogleMap
-    defaultZoom={14}
-    defaultCenter={{ lat: 34.004436, lng: -83.968197 }}
-  >
-    <Marker
-      position={{ lat: 34.004436, lng: -83.968197 }}
-      onClick={props.toggleInfoWindow}
+    <GoogleMap
+        defaultZoom={zoomLevel}
+        defaultCenter={{ lat: 34.004436, lng: -83.968197 }}
     >
-      {props.isOpen &&
-        <InfoWindow onCloseClick={props.toggleInfoWindow}>
-          <MapLabel />
-        </InfoWindow>}
-    </Marker>
-  </GoogleMap>
+        <Marker
+            position={{ lat: 34.004436, lng: -83.968197 }}
+            onClick={props.toggleInfoWindow}
+        >
+            {props.isOpen &&
+                <InfoWindow onCloseClick={props.toggleInfoWindow}>
+                    <MapLabel />
+                </InfoWindow>}
+        </Marker>
+    </GoogleMap>
 );
 
 export default LocationMap;
