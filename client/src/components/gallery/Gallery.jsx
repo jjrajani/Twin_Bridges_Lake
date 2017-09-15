ee7959e97a747b9f07ead60beba212e3e9fcbd5d
humdrum <jjrajani@gmail.com>
2017-09-15 11:44:17 -0400

    landscape view picture height

--- client/src/components/gallery/Gallery.jsx
+++ client/src/components/gallery/Gallery.jsx
@@ -1,56 +1,57 @@
-//72157689006206145
-
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
 
 class Gallery extends Component {
-  componentDidMount() {
-    this.props.fetchPics();
-  }
-  render() {
-    return (
-      <div className="gallery">
-        <div className="row">
-          <div className="col-xs-12">
-            <div className="logo">
-              <i className="fa fa-camera-retro" />
-              <h1>Gallery</h1>
+    componentDidMount() {
+        this.props.fetchPics();
+    }
+    render() {
+        return (
+            <div className="gallery">
+                <div className="row">
+                    <div className="col-xs-12">
+                        <div className="logo">
+                            <i className="fa fa-camera-retro" />
+                            <h1>Gallery</h1>
+                        </div>
+                        <p>
+                            Checkout some of the fish that were caught at Twin
+                            Bridges Lake.
+                        </p>
+                        <p>
+                            Email pictures of your catches to{' '}
+                            <a href="mailto:twin.bridges.lake.fishing@gmail.com">
+                                twin.bridges.lake.fishing@gmail.com
+                            </a>{' '}
+                            and we will add them to the gallery!
+                        </p>
+                    </div>
+                    <div className="col-xs-12">
+                        <ul className="image_list">
+                            {this.props.pics.map(p => {
+                                return (
+                                    <a
+                                        href={p}
+                                        className="img_wrapper col-xs-12 col-sm-6 col-md-4"
+                                        target="blank"
+                                    >
+                                        <li
+                                            key={p}
+                                            className="image_item"
+                                            style={{
+                                                backgroundImage: `url(${p})`
+                                            }}
+                                        />
+                                    </a>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <p>
-              Checkout some of the fish that were caught at Twin Bridges Lake.
-            </p>
-            <p>
-              Email pictures of your catches to{' '}
-              <a href="mailto:twin.bridges.lake.fishing@gmail.com">
-                twin.bridges.lake.fishing@gmail.com
-              </a>{' '}
-              and we will add them to the gallery!
-            </p>
-          </div>
-          <div className="col-xs-12">
-            <ul className="image_list">
-              {this.props.pics.map(p => {
-                return (
-                  <a
-                    href={p}
-                    className="img_wrapper col-xs-12 col-sm-6 col-md-4"
-                    target="blank"
-                  >
-                    <li
-                      key={p}
-                      className="image_item"
-                      style={{ backgroundImage: `url(${p})` }}
-                    />
-                  </a>
-                );
-              })}
-            </ul>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 // const Gallery = ({ fetchPics, pics, selectImage }) => {
 //   return (
@@ -69,11 +70,11 @@ class Gallery extends Component {
 // };
 
 function mapStateToProps({ pics }) {
-  return { pics };
+    return { pics };
 }
 
 export default connect(mapStateToProps, {
-  fetchPics: actions.picsActions.fetchPics,
-  selectImage: actions.galleryActions.selectImage
+    fetchPics: actions.picsActions.fetchPics,
+    selectImage: actions.galleryActions.selectImage
 })(Gallery);
 //onClick={selectImage.bind(this, p)}
