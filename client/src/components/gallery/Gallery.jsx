bdc7560b643c4373631f5acefe77309d598051c7
humdrum <jjrajani@gmail.com>
2017-09-15 11:49:08 -0400

    gallery link, image height

--- client/src/components/gallery/Gallery.jsx
+++ client/src/components/gallery/Gallery.jsx
@@ -34,7 +34,6 @@ class Gallery extends Component {
                                     <a
                                         href={p}
                                         className="img_wrapper col-xs-12 col-sm-6 col-md-4"
-                                        target="blank"
                                     >
                                         <li
                                             key={p}
