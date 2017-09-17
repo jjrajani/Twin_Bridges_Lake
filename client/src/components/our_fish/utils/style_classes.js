81aa5062dcf59b1b01a6bd6c5aac23133d575364
humdrum <jjrajani@gmail.com>
2017-09-16 21:40:57 -0400

    channel cat style_class to flip image

--- client/src/components/our_fish/utils/style_classes.js
+++ client/src/components/our_fish/utils/style_classes.js
@@ -2,10 +2,13 @@
 const WRAPPER_CLASS = 'col-xs-12 col-sm-6 col-md-4 fish_wrapper';
 const BREAM_CLASS = 'col-xs-12 col-sm-6 col-md-4 fish_wrapper bream';
 const CRAPPIE_CLASS = 'col-xs-12 col-sm-6 col-md-4 fish_wrapper crappie';
+const CHANNEL_CLASS = 'col-xs-12 col-sm-6 col-md-4 fish_wrapper channel';
 
 const applyClass = fish =>
     fish.text === 'Bream'
         ? BREAM_CLASS
-        : fish.text === 'Crappie' ? CRAPPIE_CLASS : WRAPPER_CLASS;
+        : fish.text === 'Crappie'
+            ? CRAPPIE_CLASS
+            : fish.text === 'Channel Cat' ? CHANNEL_CLASS : WRAPPER_CLASS;
 
 export default applyClass;
