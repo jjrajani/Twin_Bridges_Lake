95a1945f82d4c0457c108f9b91d5a4972746e080
humdrum <jjrajani@gmail.com>
2017-09-16 20:35:14 -0400

    gallery refac

--- client/src/components/gallery/Gallery.jsx
+++ client/src/components/gallery/Gallery.jsx
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import React from 'react';
 import { GalleryHeader, ImageList } from './components';
 
 const Gallery = () =>
